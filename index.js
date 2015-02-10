6949dc59d3e603ffc692d334df715739aabb53f8
griffio <griffio@users.noreply.github.com>
2015-02-10 02:37:28 -0700

    added baconjs for simple de-duping example

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 var Hapi = require("hapi");
 var Bleacon = require("bleacon");
 var Socketio = require("socket.io");
+var Bacon = require("baconjs").Bacon;
+
 var server= new Hapi.Server();
 server.connection({port:8080});
 var io = Socketio.listen(server.listener);
+io.sockets.on("connection", function(socket) {
 
-var beaconService = function emitBeacon(socket) {
+    var discoveries = Bacon.fromBinder(function(sink) {
+        Bleacon.on('discover', function(bleacon){
+            sink(bleacon);
+        });
+    });
+
+    discoveries.skipDuplicates(function(oldval, newval) {
+        return oldval.uuid === newval.uuid
+            && oldval.major === newval.major
+            && oldval.minor === newval.minor
+            && oldval.proximity === newval.proximity;
+    }).onValue(function(bleacon) {
+        socket.emit("beacon", bleacon);
+    });
+
     Bleacon.startScanning();
-    Bleacon.on('discover', function(bleacon) {
-        console.log(JSON.stringify(bleacon));
-        socket.emit('beacon', bleacon);    
-    });        
-};
 
-io.sockets.on("connection", beaconService);
+});
 
 server.route({
     path: "/{static*}",
